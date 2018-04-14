d2d56ed203fcf85dbf34ecdf0771217add99d062
kaktuxmx <kaktuxsql@gmail.com>
2018-04-13 18:43:13 -0700

    Merge server and models

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+
+var PORT = process.env.PORT || 8000;
+
+var db = require("./models");
+
+var app = express();
+
